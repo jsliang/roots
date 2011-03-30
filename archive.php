c3ee4d97dc20eea69e04b264df773657c0e09371
Jui-Shan Liang <jsliang.tw@gmail.com>
2011-03-30 21:39:40 +0800

    Modified archive display template (category -> single)

--- archive.php
+++ archive.php
@@ -3,7 +3,8 @@
 			<div id="main" class="<?php echo get_option('roots_main_class'); ?>" role="main">
 				<div class="container">
 					<h1><?php single_cat_title(); ?></h1>
-					<?php get_template_part('loop', 'category'); ?>
+					<hr/>
+					<?php get_template_part('loop', 'single'); ?>
 				</div>
 			</div><!-- /#main -->
 			<aside id="sidebar" class="<?php echo get_option('roots_sidebar_class'); ?>" role="complementary">
